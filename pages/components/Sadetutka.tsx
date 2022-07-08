db4ae719882f5e1afa9753ae1577235d468c6df8
Antti <anttituomola8@gmail.com>
2022-07-08 13:25:59 +0300

    rain map embeded

--- pages/components/Sadetutka.tsx
+++ pages/components/Sadetutka.tsx
@@ -1,7 +1,9 @@
 type Props = {}
 const Sadetutka = (props: Props) => {
     return (
-        <div>sadetutka</div>
+        <div className="sadetutka">
+            <iframe src="https://www.sataako.fi/?x=2646141.634220&y=8742179.362004&zoom=13&collapsed=true"></iframe>
+        </div>
     )
 }
 export default Sadetutka
