017138515a952aec32dbeb50908f7b0b46570923
Antti <anttituomola8@gmail.com>
2022-07-11 10:21:06 +0300

    removed unused types

--- components/Perhekalenteri.tsx
+++ components/Perhekalenteri.tsx
@@ -1,5 +1,4 @@
-type Props = {}
-const Perhekalenteri = (props: Props) => {
+const Perhekalenteri = () => {
     return (
         <div className="perhekalenteri">
             <iframe className="perhekalenteriEl" src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FHelsinki&showCalendars=0&showTitle=0&mode=AGENDA&showNav=1&showDate=0&showPrint=0&showTabs=0&showTz=0&src=OHY4ZjlnbDRtYW1zamFkYzdsMWsxM2xrOTRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23EF6C00"></iframe>
--- components/Sadetutka.tsx
+++ components/Sadetutka.tsx
@@ -1,5 +1,4 @@
-type Props = {}
-const Sadetutka = (props: Props) => {
+const Sadetutka = () => {
     return (
         <div className="sadetutka">
             <iframe className="sadetutkaEl" src="https://www.sataako.fi/?x=2646141.634220&y=8742179.362004&zoom=12&collapsed=true"></iframe>
